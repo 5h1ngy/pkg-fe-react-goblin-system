b26ff5bab9f3b91fc6dd0e59454aa45fc8674055
5h1ngy <sig.scarano@outlook.it>
2025-02-13 07:10:36 +0100

    chore(arch): :chart_with_upwards_trend: rollup builds test

--- rollup.config.js
+++ rollup.config.js
@@ -2,50 +2,49 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import json from '@rollup/plugin-json'
-import typescript from '@rollup/plugin-typescript'
+// import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts'
 
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
-  // Primo oggetto: bundle .js (CJS + ESM)
   {
-    input: 'src/index.ts',   // entry point del tuo pacchetto
+    input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,   // es: "dist/index.cjs.js"
+        file: packageJson.main,
         format: 'cjs',
         sourcemap: true,
       },
       {
-        file: packageJson.module, // es: "dist/index.esm.js"
+        file: packageJson.module,
         format: 'esm',
         sourcemap: true,
       },
     ],
-    // Riconosce le dipendenze (React, Chakra, etc.) come peer
     external: [
       ...Object.keys(packageJson.peerDependencies || {}),
-      // In genere si includono anche le 'dependencies' se vuoi escluderle dal bundle
-      // ...Object.keys(packageJson.dependencies || {})
     ],
     plugins: [
       peerDepsExternal(),
-      resolve(),
+      resolve({
+        extensions: ['.js', '.ts', '.tsx'],
+      }),
       commonjs(),
       json(),
       typescript({
-        tsconfig: './tsconfig.json',  // Assicurati punti al tuo tsconfig
+        tsconfig: './tsconfig.app.json',
+        include: ['src/**/*'],
+        exclude: ['node_modules', '**/*.stories.tsx', '**/*.test.tsx'],
       }),
     ],
   },
 
-  // Secondo oggetto: generazione file .d.ts (tipi)
   {
     input: 'dist/types/src/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    // Imposta "external" per evitare di includere tipi di React e simili
     external: [
       ...Object.keys(packageJson.peerDependencies || {}),
     ],
--- src/components/Factory/DynamicForm/shared/hooks.tsx
+++ src/components/Factory/DynamicForm/shared/hooks.tsx
@@ -3,42 +3,18 @@ import { TypeOf, z, ZodObject } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { MetaSchema } from '../dynamicForm.types';
+import { Fields } from './hooks.types';
 
-/**
- * @typedef {Array<Record<string, [string, unknown][]>>} Fields
- * Represents the structured fields data. Each step is an array of rows, 
- * where each row is a record of field name and schema tuples.
- */
-export type Fields = Array<Record<string, [string, unknown][]>>
+export function useNormalizeFields<T extends z.ZodEffects<ZodObject<any>>>(
+    schema: T,
+    defaultValues?: DefaultValues<TypeOf<T>> | undefined
+) {
 
-/**
- * Normalizes form fields based on a Zod schema for use with react-hook-form.
- * 
- * @template T - A Zod schema object.
- * @param {T} schema - The Zod schema defining the structure and metadata of the form.
- * @returns {Object} - An object containing:
- * - `fields`: The normalized field structure organized by steps and rows.
- * - `methods`: React Hook Form methods bound to the Zod schema.
- * 
- * @example
- * ```typescript
- * const schema = z.object({
- *   name: z.string().meta({ row: 1, step: 0 }),
- *   age: z.number().meta({ row: 2, step: 0 })
- * });
- * 
- * const { fields, methods } = useNormalizeFields(schema);
- * ```
- */
-export function useNormalizeFields<T extends z.ZodEffects<ZodObject<any>>>(schema: T, defaultValues?: DefaultValues<TypeOf<T>> | undefined) {
-
-    // Initialize react-hook-form with a Zod resolver
     const methods = useForm<z.infer<T>>({
         resolver: zodResolver(schema),
         defaultValues,
     });
 
-    // Normalize the schema's shape into a structured field object
     const fields = schema.innerType().shape
         ? Object.entries(schema.innerType().shape)
             .reduce((fields: Fields, [name, fieldSchema]) => {
