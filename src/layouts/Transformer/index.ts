325c2b91d36b9aaef0c78c342dea79de0daf4b6f
5h1ngy <sig.scarano@outlook.it>
2025-02-20 04:38:59 +0100

    fix: :art: layout Transformer export type

--- src/layouts/Transformer/index.ts
+++ src/layouts/Transformer/index.ts
@@ -1,4 +1,6 @@
 import withContext from "./shared/Provider";
 import Transformer from "./Transformer";
+import { ComponentProps } from "./transformer.types"
 
 export default withContext(Transformer)
+export type { ComponentProps }
