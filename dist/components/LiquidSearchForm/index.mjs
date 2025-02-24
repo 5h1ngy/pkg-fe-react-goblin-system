22caea5525e65dd2d78a5ae3f51812efede89170
5h1ngy <sig.scarano@outlook.it>
2025-02-24 19:33:55 +0100

    style(components): ✨ dynamic form css

--- dist/components/LiquidSearchForm/index.mjs
+++ dist/components/LiquidSearchForm/index.mjs
@@ -1,5 +1,5 @@
 import { j as jsxRuntimeExports } from "../../.chunks/CSg68eI9.js";
-import { VStack, Stack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import { D as DynamicForm } from "../../.chunks/Clcub7Py.js";
 import { Button } from "../Factory/Chakra/button.mjs";
 function LiquidSearchForm({ schema }) {
@@ -8,7 +8,7 @@ function LiquidSearchForm({ schema }) {
     {
       schema,
       width: "full",
-      render: (fields) => /* @__PURE__ */ jsxRuntimeExports.jsx(VStack, { children: /* @__PURE__ */ jsxRuntimeExports.jsxs(
+      render: (fields) => /* @__PURE__ */ jsxRuntimeExports.jsxs(
         Stack,
         {
           width: "full",
@@ -20,10 +20,10 @@ function LiquidSearchForm({ schema }) {
           _dark: { backgroundColor: "gray.950" },
           children: [
             fields,
-            /* @__PURE__ */ jsxRuntimeExports.jsx(Stack, { width: "fit-content", alignItems: "flex-end", children: /* @__PURE__ */ jsxRuntimeExports.jsx(Button, { variant: "outline", type: "submit", colorPalette: "teal", children: "Crea Account" }) })
+            /* @__PURE__ */ jsxRuntimeExports.jsx(Button, { variant: "outline", type: "submit", colorPalette: "teal", children: "Crea Account" })
           ]
         }
-      ) })
+      )
     }
   );
 }
--- src/components/LiquidSearchForm/LiquidSearchForm.tsx
+++ src/components/LiquidSearchForm/LiquidSearchForm.tsx
@@ -1,4 +1,4 @@
-import { Stack, VStack } from "@chakra-ui/react"
+import { Stack } from "@chakra-ui/react"
 import { z, ZodObject } from 'zod';
 
 import DynamicForm from '@/components/Factory/DynamicForm/DynamicForm';
@@ -12,19 +12,15 @@ function LiquidSearchForm<T extends z.ZodEffects<ZodObject<any>>>({ schema }: Co
         schema={schema}
         width={"full"}
         render={(fields: React.ReactNode[]) =>
-            <VStack>
-                <Stack width={"full"} wrap={"wrap"}
-                    gap={"1rem"} padding={"1.5rem"} borderRadius={"10px"}
-                    backgroundColor={"gray.100"} _dark={{ backgroundColor: "gray.950", }}
-                >
-                    {fields}
-                    <Stack width={"fit-content"} alignItems={"flex-end"}>
-                        <Button variant="outline" type="submit" colorPalette={"teal"}>
-                            Crea Account
-                        </Button>
-                    </Stack>
-                </Stack>
-            </VStack>
+            <Stack width={"full"} wrap={"wrap"}
+                gap={"1rem"} padding={"1.5rem"} borderRadius={"10px"}
+                backgroundColor={"gray.100"} _dark={{ backgroundColor: "gray.950", }}
+            >
+                {fields}
+                <Button variant="outline" type="submit" colorPalette={"teal"}>
+                    Crea Account
+                </Button>
+            </Stack>
         }
     />
 }
