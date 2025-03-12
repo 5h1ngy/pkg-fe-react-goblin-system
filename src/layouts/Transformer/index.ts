550562cfaaf6315757fd2874c51912a46fb539ba
5h1ngy <sig.scarano@outlook.it>
2025-03-12 23:30:28 +0100

    feat: :sparkles: Tranformer footer system fix usage

--- src/layouts/Transformer/index.ts
+++ src/layouts/Transformer/index.ts
@@ -1,6 +1,8 @@
+import { useFooter } from "./shared/hooks";
 import withContext from "./shared/Provider";
 import Transformer from "./Transformer";
 import { ComponentProps } from "./transformer.types"
 
 export default withContext(Transformer)
 export type { ComponentProps }
+export { useFooter }
